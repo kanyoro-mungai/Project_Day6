e6a61fca41bc9b3c556108d95cdc627d0761074d
SnyderMbishai <csmbishai@gmail.com>
2017-05-12 14:28:42 +0300

    a create events function that returns the event after the user inputs a certain date

--- calender.py
+++ calender.py
@@ -1,14 +1,14 @@
-class Calender:
-
-	def create user():
-
-	def createcalender():
-
-	def create events():
-
-	def viewevents():
-
-	def viewlastevent():
-		
-
-
+def create events():
+		raw_input("enter the Calender date")
+		if Calender_date==1 :
+			return "today`s event is Self Learning(gitHub)"
+		elif Calender_date==2:
+			return "today`s event is Self Learning (Test Driven Developements)"
+		elif Calender_date==3:
+			return "today`s events are profesional writting"
+		elif Calender_date==4:
+			return"Today`s event is Slack interaction- collaboration enhensement"
+		elif Calender_date==5:
+			return "Today`s event is Meet Up for an application building"
+		else:
+			return NONE
