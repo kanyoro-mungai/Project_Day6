feffd35e1b1c8e2c41241dcfcb9e2bbb40fe595b
SnyderMbishai <csmbishai@gmail.com>
2017-05-12 14:48:01 +0300

    a program that takes c_name variable as created by munyoro for efficiency in running

--- calender.py
+++ calender.py
@@ -1,14 +1,14 @@
 def create events():
 		raw_input("enter the Calender date")
-		if Calender_date==1 :
+		if c_name==1 :
 			return "today`s event is Self Learning(gitHub)"
-		elif Calender_date==2:
+		elif c_name==2:
 			return "today`s event is Self Learning (Test Driven Developements)"
-		elif Calender_date==3:
+		elif c_name==3:
 			return "today`s events are profesional writting"
-		elif Calender_date==4:
+		elif c_name==4:
 			return"Today`s event is Slack interaction- collaboration enhensement"
-		elif Calender_date==5:
+		elif c_name==5:
 			return "Today`s event is Meet Up for an application building"
 		else:
 			return NONE
