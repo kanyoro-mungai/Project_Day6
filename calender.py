1ad69ec1e905d9571d7e969f0bd1cfb023c2db6f
BMNyabuto <bnyabuto@gmail.com>
2017-05-12 13:34:51 +0300

    updated create user function

--- calender.py
+++ calender.py
@@ -1,6 +1,17 @@
 class Calender:
 
-	def create user():
+	def createuser():
+		user = {name:b_no}
+
+		name = raw_input("Please enter your name: ")
+			print "you entered", name
+
+		b_no = raw_input("Please enter your bootcamp id: ")
+			print "you entered", b_no
+
+		user.update({name:b_no})
+
+		return name
 
 	def createcalender():
 
